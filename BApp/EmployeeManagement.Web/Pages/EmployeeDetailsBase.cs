174df50e2df40de98de7daed781425cf992f0cd8
Mjauski <kikskolesnik@gmail.com>
2021-06-17 22:51:00 +0300

    24. Errorid (ja eelmistest jäi midagi stagemata)

--- BApp/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ BApp/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Web.Pages
@@ -8,6 +9,25 @@ namespace EmployeeManagement.Web.Pages
     public class EmployeeDetailsBase : ComponentBase
     {
         public Employee Employee { get; set; } = new Employee();
+
+        protected string Coordinates { get; set; }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
 
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
@@ -20,5 +40,10 @@ namespace EmployeeManagement.Web.Pages
             Id = Id ?? "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
-    }
+
+        //protected void Mouse_Move(MouseEventArgs e)
+        //{
+          //  Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        //}
+
 }
