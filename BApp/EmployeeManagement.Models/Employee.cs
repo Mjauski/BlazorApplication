ecd90708dbaca5a6ef37b5dff2853585e11e68a8
Mjauski <kikskolesnik@gmail.com>
2021-06-20 23:00:53 +0300

    36.

--- BApp/EmployeeManagement.Models/Employee.cs
+++ BApp/EmployeeManagement.Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagement.Models.CustomValidators;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -13,6 +14,9 @@ namespace EmployeeManagement.Models
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "pragimtech.com",
+            ErrorMessage = "OnlyPragimTech.com is allowed")]
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
